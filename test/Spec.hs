80060cade033cbfe48ab8124c0daad6426baaf2b
05st <stimsina@protonmail.com>
2023-04-09 14:58:13 -0500

    Improve test spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,8 +6,21 @@ import Control.Monad
 import System.IO
 import System.IO.Silently
 import System.Directory
+import System.FilePath
 
 import qualified Fino as F
+
+goldenTest :: String -> String -> Golden String
+goldenTest name out =
+    Golden {
+        output = out,
+        encodePretty = show,
+        writeToFile = writeFile,
+        readFromFile = readFile,
+        goldenFile = ".test" </> name </> "expected",
+        actualFile = Just (".test" </> name </> "actual"),
+        failFirstTime = False
+    }
 
 basePath :: FilePath
 basePath = "test/cases/"
@@ -15,17 +28,18 @@ basePath = "test/cases/"
 spec :: Spec
 spec = do
     items <- runIO (listDirectory basePath)
-    forM_ items $ \path -> do
-        before (runTest (basePath ++ path ++ "/")) $ describe "" $ do
-            it ("case: " ++ path) $ \res -> do
-                defaultGolden
-                    ("case-" ++ path)
-                    res
+    describe "Golden Tests" $ do
+        forM_ items $ \path -> do
+            before (runTest (basePath ++ path ++ "/")) $ do
+                it ("case: " ++ path) $ \res -> do
+                    goldenTest
+                        ("case-" ++ path)
+                        res
 
 runTest :: [Char] -> IO String
 runTest path = do
     let opts = F.Options path "" False
-    fst <$> hCapture [stderr] (F.runOptions opts)
+    fst <$> hCapture [stderr] (silence (F.runOptions opts))
 
 main :: IO ()
 main = hspec spec
